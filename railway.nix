d4222c5aab2d7ab548e9b0a4a45f11abec842153
omarsameh-1 <49820429+omarsameh-1@users.noreply.github.com>
2025-02-28 23:32:08 +0200

    chaning nix

--- railway.nix
+++ railway.nix
@@ -1,7 +1,7 @@
 { pkgs }: {
   deps = [
-    pkgs.php74
-    pkgs.composer
+    (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1a57b9e2e9eb1934e870c192b463cb6204f5df2e.tar.gz") {}).php80  # PHP 8.0.2 from an older Nix package
+    (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1a57b9e2e9eb1934e870c192b463cb6204f5df2e.tar.gz") {}).php80Packages.composer  # Correct Composer version
     pkgs.curl
     pkgs.git
     pkgs.unzip
